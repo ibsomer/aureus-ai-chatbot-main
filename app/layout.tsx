42dd206c74bb5e0cf93228864a0d66875c07fa12
ibsomer <ibsomer1@gmail.com>
2025-05-27 00:03:37 +0100

    Update: Added customized Settings page (page.tsx)

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                     <Navbar
                       setApiKey={setApiKey}
                       onOpen={onOpen}
-                      logoText={'Horizon UI Dashboard PRO'}
+                      logoText={'Aureus AI UI Dashboard PRO'}
                       brandText={getActiveRoute(routes, pathname)}
                       secondary={getActiveNavbar(routes, pathname)}
                     />
