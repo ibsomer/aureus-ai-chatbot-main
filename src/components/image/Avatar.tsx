0d88c36c7bd3dedb21fbb538c08298aadf182ef5
ibsomer <ibsomer1@gmail.com>
2025-05-26 21:56:17 +0100

    Fix Avatar.tsx objectFit issue

--- src/components/image/Avatar.tsx
+++ src/components/image/Avatar.tsx
@@ -1,41 +1,44 @@
-'use client'
+'use client';
 import { chakra, useColorMode } from '@chakra-ui/system';
 import { ComponentProps } from 'react';
 import { Image } from './Image';
+import type { ResponsiveValue } from '@chakra-ui/styled-system';
 
 type AvatarImageProps = Partial<
-    ComponentProps<typeof Image> & {
-        showBorder?: boolean;
-    }
+  ComponentProps<typeof Image> & {
+    showBorder?: boolean;
+    objectFit?: ResponsiveValue<'fill' | 'contain' | 'cover' | 'none' | 'scale-down'>;
+  }
 >;
 
 export function NextAvatar({
-    src,
-    showBorder,
-    alt = '',
-    style,
-    ...props
+  src,
+  showBorder,
+  alt = '',
+  style,
+  objectFit = 'fill',
+  ...props
 }: AvatarImageProps) {
-    const { colorMode } = useColorMode();
+  const { colorMode } = useColorMode();
 
-    return (
-        <Image
-            {...props}
-            {...(showBorder
-                ? {
-                      border: '2px',
-                      borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
-                  }
-                : {})}
-            alt={alt}
-            objectFit={'fill'}
-            src={src}
-            style={{ ...style, borderRadius: '50%' }}
-        />
-    );
+  return (
+    <Image
+      {...props}
+      {...(showBorder
+        ? {
+            border: '2px',
+            borderColor: colorMode === 'dark' ? 'navy.700' : 'white',
+          }
+        : {})}
+      alt={alt}
+      objectFit={objectFit}
+      src={src}
+      style={{ ...style, borderRadius: '50%' }}
+    />
+  );
 }
 
 export const ChakraNextAvatar = chakra(NextAvatar, {
   shouldForwardProp: (prop) =>
-    ['width', 'height', 'src', 'alt', 'layout'].includes(prop),
+    ['width', 'height', 'src', 'alt', 'layout', 'objectFit'].includes(prop),
 });
